02a9bac054c261dbf6754f1f7e8a0461fba5ce6f
叶昊炜 <yehaowei666@163.com>
2018-10-26 14:52:38 +0800

    React.createElement add comment

--- React/createElement.js
+++ React/createElement.js
@@ -5,6 +5,7 @@
  * @param  {...any} children 子元素
  *
  * @description transform-react-jsx插件去识别jsx后，再去循环调用createElement方法
+ * @abstract 如果jsx片段中某个元素是组件，则tag将会是一个方法而不是一个字符串。区分组件和原生DOM的工作，babel-plugin-transform-react-jsx会帮我们做
  *
  */
 function createElement (tag, attrs, ...children) {
