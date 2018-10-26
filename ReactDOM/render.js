8c3d2795b0dda6575762b0a3bc139d92c541a82f
叶昊炜 <yehaowei666@163.com>
2018-10-26 16:52:51 +0800

    update ReactDOM.render

--- ReactDOM/render.js
+++ ReactDOM/render.js
@@ -9,27 +9,39 @@ import setAttribute from './setAttribute'
  *
  */
 function render (vnode, container) {
-  // 当vnode为字符串时，渲染结果是一段文本
-  if (typeof vnode === 'string') {
-    const textNode = document.createTextNode(vnode)
+  // 当vnode为undefined、null、boolean时，渲染结果为''
+  if (vnode === undefined || vnode === null || typeof vnode === 'boolean') vnode = ''
+
+  // 当vnode为string、number时，渲染结果是一段文本
+  if (typeof vnode === 'string' || typeof vnode === 'number') {
+    const textNode = document.createTextNode(String(vnode))
     return container.appendChild(textNode)
   }
 
   const { tag, attrs, children } = vnode
+  let dom = null
 
-  // 创建DOM
-  const dom = document.createElement(tag)
+  // 当vnode.tag为react component时
+  if (typeof tag === 'function') {
+    const component = createComponent(tag, attrs)
+    setComponentProps(component, attrs)
+    dom = component.base
+  // 当vnode.tag为普通HTML元素标签时
+  } else {
+    // 创建DOM
+    dom = document.createElement(tag)
 
-  // 遍历attrs
-  if (attrs) {
-    Object.keys(attrs).forEach(key => {
-      const value = attrs[key]
-      setAttribute(dom, key, value) // 调用setAttribute函数，设置属性
-    })
+    // 遍历attrs
+    if (attrs) {
+      Object.keys(attrs).forEach(key => {
+        const value = attrs[key]
+        setAttribute(dom, key, value) // 调用setAttribute函数，设置属性
+      })
+    }
+
+    // 遍历children，递归render
+    children.forEach(child => render(child, dom))
   }
-
-  // 遍历children，递归render
-  children.forEach(child => render(child, dom))
 
   // 将渲染结果挂载到真实的DOM容器中
   return container.appendChild(dom)
