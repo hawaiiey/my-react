a2cb74b081c1a653c4ae67e8aefcf1424f996215
叶昊炜 <yehaowei666@163.com>
2018-10-30 21:55:02 +0800

    nested component

---
+++ components/Name/index.js
@@ -0,0 +1,7 @@
+import React from '../../React'
+
+export default class Name extends React.Component {
+  render() {
+    return <span>{this.props.name}</span>
+  }
+}
--- components/Welcome/index.js
+++ components/Welcome/index.js
@@ -1,7 +1,11 @@
 import React from '../../React'
+import Name from '../Name'
 
 export default class Welcome extends React.Component {
   render() {
-    return <h1>Hello, {this.props.name}!!!</h1>
+    return <h1>
+      Hello,
+      <Name name={this.props.name} />!!!
+    </h1>
   }
 }
